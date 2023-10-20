275de67f740cf2d7184baa6a0555c51b04eeaca7
ajitesh-545 <ajitesh-tripathi@outlook.com>
2023-10-20 20:02:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+const App = () => {
+  return <h1>Hello React</h1>;
+};
+
+export default App;import React from "react";
+const App = () => {
+  return <h1>Hello!! This application is build using React, Webpack, </h1>;
+};
+
+export default App;
